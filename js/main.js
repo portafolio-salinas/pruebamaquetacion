1dd972526629b32e1ba69cd56377685e5ac4f9e7
vurles <portafolio.salinas@gmail.com>
2017-01-06 12:19:41 -0300

    slider logrado

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ $(window).load(function() {
     $(this).toggleClass("animated");
   });
 });
+$(document).ready(function() {
+ $('#slides').superslides({
+   slide_easing: 'easeInOutCubic',
+   slide_speed: 800,
+   pagination: true,
+   hashchange: true,
+   scrollable: true
+ });
+});
